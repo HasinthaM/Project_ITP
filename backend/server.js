b6b64530c8abf8a5b445c85b54b4defb753ed4cc
Dhananajayaweerakoon <128120353+Dhananajayaweerakoon@users.noreply.github.com>
2024-03-19 20:19:58 +0530

    create folders

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express(); //app -express instance
-const cors = require('cors');
+const cors = require("cors");
 const port = 3001;
-const host = 'localhost';  // can use http://localhost also
-const mongoose = require('mongoose');
-const router = require('./routers/router');
+const host = "localhost"; // can use http://localhost also
+const mongoose = require("mongoose");
+const router = require("./routers/router");
 
 app.use(cors());
 app.use(express.json());
 
-const uri = 'mongodb+srv://projectitp:itp@cluster0.90q48aw.mongodb.net/?retryWrites=true&w=majority';
+const uri =
+  "mongodb+srv://dhananjaya:mongo2213@cluster0.u3zq0ye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const connect = async () => {
-     try { 
-          await mongoose.connect(uri);
-          console.log('Connected to MongoDB');
-     }
-     catch(error){
-          console.log('MongoDB Error: ', error);
-
-     }
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("MongoDB Error: ", error);
+  }
 };
 
 connect();
 
 //create server to run node js
-const server = app.listen(port, host, () =>{
-     console.log(`Node server is listening to ${server.address().port}`)
-}); 
+const server = app.listen(port, host, () => {
+  console.log(`Node server is listening to ${server.address().port}`);
+});
 
-app.use('/api', router);
+app.use("/api", router);
