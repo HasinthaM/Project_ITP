9d57aa682249bd578ba2fa03fe514f779ed67b09
Oshadha Nipun <oshadhanipun2k1@gmail.com>
2024-04-30 12:09:14 +0530

    App.js changed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { useNavigate } from 'react-router-dom'; //Hook
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './App.css';
+
 
 function App() {
 
   const navigate = useNavigate();
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome</h1>
-        <button className='manager-dash' onClick={() => navigate('/pdashboard')}>Dashboard</button>
-        <button className='p-home' onClick={() => navigate('/phome')}>Package Home</button>
+    <div className="App1">
+      <header className="App-header1">
+        <h1>Package Managemnt</h1>
+        <button className='manager-dash' onClick={() => navigate('/pdashboard')}>Package Manager</button>
+        <button className='p-homeI' onClick={() => navigate('/phome')}>User</button>
         <button className='o-home' onClick={() => navigate('/o-manager')}>Operation Manager</button>
 
       </header>
